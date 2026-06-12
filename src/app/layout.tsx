9af56c12536ac2f9ee34d4bebeed74737b56f441
zaidmanemilio <zaidmanemilio@gmail.com>
2026-06-12 14:24:01 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang="es" data-theme="light">
       <body>{children}</body>
     </html>
   )
